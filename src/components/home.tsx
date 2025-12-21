19ab9d78d2eb0ccb4abfbcb211ffc262d717777c
= <evy1339@gmail.com>
2025-12-21 10:23:35 -0300

    add social media links and icons to home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,6 @@
 import background from "../assets/background-pc.jpg";
+import githubIco from "../assets/github-ico.png";
+import linkedinIco from "../assets/linkedin-ico.png";
 
 function Home() {
   return <div className="home-container">
@@ -7,8 +9,14 @@ function Home() {
     <h2>Desenvolvedor em Formação | Sites, jogos e aplicações interativas</h2>
     <p className="short-description">Estudante de Desenvolvimento de Sistemas focado em <b>JavaScript</b>, <b>TypeScript</b>, <b>React</b>, <b>PHP</b> e <b>python</b>, com interesse em <b>aplicações web</b>, <b>jogos</b> e <b>projetos interativos</b>. Busco unir lógica, criatividade e aprendizado contínuo em cada projeto.</p>
     <div className="home-buttons-container">
-        <a className="home-button">Linkedin</a>
-        <a className="home-button">Github</a>
+        <a href="https://www.linkedin.com/in/everton-mancio/" target="_blank" className="home-button">
+        <img src={linkedinIco} className="button-icon" />
+        Linkedin
+        </a>
+        <a href="https://github.com/Evy8882" target="_blank" className="home-button">
+        <img src={githubIco} className="button-icon invert" />
+        Github
+        </a>
     </div>
   </div>;
 }
